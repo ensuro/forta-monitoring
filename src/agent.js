58d100ad03546a54a2f6c97bc43778325990e5ef
GnP <g.parrondo@gmail.com>
2023-02-01 18:39:11 -0300

    Dependency injection on main entrypoint

--- src/agent.js
+++ src/agent.js
@@ -10,30 +10,44 @@ const handlers = {
   dummy,
 };
 
-async function handleBlock(blockEvent) {
-  const results = [];
+function createHandleBlock(getHandlers, getConfig) {
+  const handlers = getHandlers();
+  const config = getConfig();
 
-  for (const handlerName of config.enabled) {
-    const handler = handlers[handlerName];
-    if (handler === undefined) {
-      throw new Error(`Unknown handler ${handlerName}`);
+  async function handleBlock(blockEvent) {
+    const results = [];
+
+    for (const handlerName of config.enabled) {
+      const handler = handlers[handlerName];
+      if (handler === undefined) {
+        throw new Error(`Unknown handler ${handlerName}`);
+      }
+
+      results.push(handler(blockEvent));
     }
 
-    results.push(handler(blockEvent));
+    const findings = (await Promise.all(results)).flat();
+    if (findings.length > 0) {
+      console.log(
+        "Got %s findings on block %s: %s",
+        findings.length,
+        blockEvent.blockNumber,
+        findings.map((finding) => finding.description)
+      );
+    }
+    return findings;
   }
 
-  const findings = (await Promise.all(results)).flat();
-  if (findings.length > 0) {
-    console.log(
-      "Got %s findings on block %s: %s",
-      findings.length,
-      blockEvent.blockNumber,
-      findings.map((finding) => finding.description)
-    );
-  }
-  return findings;
+  return handleBlock;
 }
+
+const handleBlock = createHandleBlock(
+  () => handlers,
+  () => config
+);
 
 module.exports = {
   handleBlock,
+  createHandleBlock,
+  handlers,
 };
