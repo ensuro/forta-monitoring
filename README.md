ad6571215aac8b397e89908f23cfee6e4867f65a
gnpar <g.parrondo@gmail.com>
2023-02-06 11:47:38 -0300

    Add active exposure monitoring (#6)

--- README.md
+++ README.md
@@ -49,3 +49,32 @@ The agent behaviour can be verified with the following blocks:
 - 37720980 (4000 USDC)
 
 You can test against this blocks with `npm run block $BLOCK_NUMBER`.
+
+## Active exposure monitoring
+
+### Description
+
+Alerts when a riskmodule's activeExposure/exposurLimit ratio exceeds a certain threshold.
+
+### Alerts
+
+Describe each of the type of alerts fired by this agent
+
+- critExposure
+  - Fired when the exposure ratio is above `critThreshold`
+  - Severity is always set to "crit"
+  - Type is always set to "info"
+- warnExposure
+  - Fired when the exposure ratio is above `warnThreshold`
+  - Severity is always set to "high"
+  - Type is always set to "info"
+
+### Test Data
+
+The agent behaviour can be verified with the following blocks:
+
+- 37901700 (99% exposure on Koala V2 (0xa65c9dE776d1f30c095EFF9C775E001a1d366df8))
+- 36901700 (53% exposure on Koala V2 (0xa65c9dE776d1f30c095EFF9C775E001a1d366df8))
+- 38878342 (no alerts, 32% exposure on Koala V2 and 4% exposure on Koala Partner B)
+
+You can test against this blocks with `npm run block $BLOCK_NUMBER`.
---
+++ src/handlers/exposure.spec.js
@@ -0,0 +1,124 @@
+const { FindingSeverity, createBlockEvent, ethers } = require("forta-agent");
+
+const { createHandleBlock, createFinding } = require("./exposure");
+const config = require("../config.json");
+
+const USDC_UNIT = ethers.BigNumber.from(10).pow(
+  config.erc20Tokens.USDC.decimals
+);
+
+const block = {
+  hash: `0x${"0".repeat(64)}`,
+  timestamp: Date.now() / 1000,
+};
+
+describe("RM exposure monitoring", () => {
+  const premiumsAccounts = [
+    {
+      name: "Test RM",
+      address: "0xab8000030e0f1f0000741672d154b5a846620001",
+      warnThresh: "0.5",
+      critThresh: "0.7",
+    },
+  ];
+
+  const activeExposure = jest.fn();
+  const exposureLimit = jest.fn();
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("returns empty findings when exposure is below thresholds", async () => {
+    const contractGetter = () => ({
+      activeExposure: activeExposure.mockImplementation(() =>
+        ethers.BigNumber.from("10").mul(USDC_UNIT)
+      ),
+      exposureLimit: exposureLimit.mockImplementation(() =>
+        ethers.BigNumber.from("100").mul(USDC_UNIT)
+      ),
+    });
+
+    const handleBlock = createHandleBlock(
+      () => {},
+      premiumsAccounts,
+      contractGetter
+    );
+
+    const blockEvent = createBlockEvent({ block: block });
+
+    const findings = await handleBlock(blockEvent);
+
+    expect(findings).toStrictEqual([]);
+    expect(activeExposure).toHaveBeenCalledWith({
+      blockTag: blockEvent.blockNumber,
+    });
+    expect(exposureLimit).toHaveBeenCalledWith({
+      blockTag: blockEvent.blockNumber,
+    });
+  });
+
+  it("returns high severity finding when exposure is above warn threshold", async () => {
+    const contractGetter = () => ({
+      activeExposure: activeExposure.mockImplementation(() =>
+        ethers.BigNumber.from("55").mul(USDC_UNIT)
+      ),
+      exposureLimit: exposureLimit.mockImplementation(() =>
+        ethers.BigNumber.from("100").mul(USDC_UNIT)
+      ),
+    });
+
+    const handleBlock = createHandleBlock(
+      () => {},
+      premiumsAccounts,
+      contractGetter
+    );
+
+    const blockEvent = createBlockEvent({ block: block });
+
+    const findings = await handleBlock(blockEvent);
+
+    expect(findings).toStrictEqual([
+      createFinding(
+        "warnExposure",
+        "High Exposure",
+        FindingSeverity.High,
+        premiumsAccounts[0],
+        "warnThresh",
+        ethers.utils.parseUnits("0.55")
+      ),
+    ]);
+  });
+
+  it("returns critical severity finding when exposure is above crit threshold", async () => {
+    const contractGetter = () => ({
+      activeExposure: activeExposure.mockImplementation(() =>
+        ethers.BigNumber.from("81").mul(USDC_UNIT)
+      ),
+      exposureLimit: exposureLimit.mockImplementation(() =>
+        ethers.BigNumber.from("100").mul(USDC_UNIT)
+      ),
+    });
+
+    const handleBlock = createHandleBlock(
+      () => {},
+      premiumsAccounts,
+      contractGetter
+    );
+
+    const blockEvent = createBlockEvent({ block: block });
+
+    const findings = await handleBlock(blockEvent);
+
+    expect(findings).toStrictEqual([
+      createFinding(
+        "critExposure",
+        "Critically high Exposure",
+        FindingSeverity.Critical,
+        premiumsAccounts[0],
+        "critThresh",
+        ethers.utils.parseUnits("0.81")
+      ),
+    ]);
+  });
+});
